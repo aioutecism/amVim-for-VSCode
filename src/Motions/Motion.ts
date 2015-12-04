815c25c993e91219ab09bf17debf0506c95728fe
aioute Gao <aioutecism@gmail.com>
2015-12-04 18:25:42 +0900

    Create Motion.

---
+++ src/Motions/Motion.ts
@@ -0,0 +1,7 @@
+export class Motion {
+	private _characterOffset: number = 0;
+	private _lineOffset: number = 0;
+
+	get characterOffset() { return this._characterOffset; }
+	get lineOffset() { return this._lineOffset; }
+}
