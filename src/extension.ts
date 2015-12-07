409736b2d7ccc265527a2a00662148625bc681f3
aioute Gao <aioutecism@gmail.com>
2015-12-07 15:33:24 +0900

    Catch number and $.

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,9 @@ export function activate(context: ExtensionContext) {
 	const dispatcher = new Dispatcher();
 	context.subscriptions.push(dispatcher);
 
-	const keys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const keys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+				+ '1234567890'
+				+ '$';
 	for (var i = 0; i < keys.length; i++) {
 		var key = keys[i];
 		context.subscriptions.push(commands.registerCommand(`vim.${key}`, dispatcher.inputHandler(key)));
