227a7c3abb241e60504ee03879de0b26421b859f
aioute Gao <aioutecism@gmail.com>
2015-12-07 17:49:19 +0900

    Bind all posiable keys.

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,35 @@ export function activate(context: ExtensionContext) {
 	const dispatcher = new Dispatcher();
 	context.subscriptions.push(dispatcher);
 
-	const keys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-				+ '1234567890'
-				+ '$';
+	let keys = [
+		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+		'1234567890',
+		'#$%*=@^_`~,./:;?',
+		'()[]{}<>\'"',
+	].join('').split('');
+
+	keys.push(
+		'space',
+		'enter',
+		'escape',
+		'backspace',
+
+		'alt+b',
+		'alt+w',
+
+		'ctrl+[',
+		'ctrl+b',
+		'ctrl+d',
+		'ctrl+e',
+		'ctrl+f',
+		'ctrl+r',
+		'ctrl+u',
+		'ctrl+w',
+		'ctrl+y',
+
+		'shift+enter'
+	);
+
 	for (var i = 0; i < keys.length; i++) {
 		var key = keys[i];
 		context.subscriptions.push(commands.registerCommand(`vim.${key}`, dispatcher.inputHandler(key)));
