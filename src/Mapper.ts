ecbf3cac642c72f0adc6172b9f6d0be7c9fa9901
aioute Gao <aioutecism@gmail.com>
2015-12-07 15:51:47 +0900

    Use string as keys.

--- src/Mapper.ts
+++ src/Mapper.ts
@@ -3,43 +3,31 @@ export interface Command {
 }
 
 export interface Map {
-	keys?: string[];
+	keys: string;
 	command: Command;
 	args?: {};
 }
 
 export class Mapper {
-	private separator = ' ';
 	private maps: {[keys: string]: Map} = {};
 
-	private key(inputs: string[]): string {
-		return inputs.join(this.separator);
-	}
-
-	map(keys: string[], command: Command, args?: {}): void {
-		this.maps[this.key(keys)] = {
+	map(keys: string, command: Command, args?: {}): void {
+		this.maps[keys] = {
+			keys: keys,
 			command: command,
 			args: args || {},
 		};
 	}
 
-	unmap(keys: string[]): void {
-		delete this.maps[this.key(keys)];
+	unmap(keys: string): void {
+		delete this.maps[keys];
 	}
 
 	match(inputs: string[]): Map {
-		let map = this.maps[this.key(inputs)];
+		let map = this.maps[inputs.join(' ')];
 
 		// TODO: part match
 
-		if (map) {
-			return {
-				command: map.command,
-				args: map.args,
-			}
-		}
-		else {
-			return null;
-		}
+		return map;
 	}
 }
--- src/Modes/Normal.ts
+++ src/Modes/Normal.ts
@@ -4,13 +4,13 @@ import {ActionMoveCursor} from '../Actions/MoveCursor';
 
 export class ModeNormal extends Mode {
 	private maps: Map[] = [
-		{ keys: ['h'], command: ActionMoveCursor.characterLeft },
-		{ keys: ['l'], command: ActionMoveCursor.characterRight },
-		{ keys: ['k'], command: ActionMoveCursor.characterUp },
-		{ keys: ['j'], command: ActionMoveCursor.characterDown },
+		{ keys: 'h', command: ActionMoveCursor.characterLeft },
+		{ keys: 'l', command: ActionMoveCursor.characterRight },
+		{ keys: 'k', command: ActionMoveCursor.characterUp },
+		{ keys: 'j', command: ActionMoveCursor.characterDown },
 
-		{ keys: ['0'], command: ActionMoveCursor.lineStart },
-		{ keys: ['$'], command: ActionMoveCursor.lineEnd },
+		{ keys: '0', command: ActionMoveCursor.lineStart },
+		{ keys: '$', command: ActionMoveCursor.lineEnd },
 	];
 
 	constructor() {
