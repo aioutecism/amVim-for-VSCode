e2b1c5c0a23bf1380627cb927f406dcac2eaba33
aioute Gao <aioutecism@gmail.com>
2015-12-16 18:46:19 +0900

    Implement ActionSelection.

---
+++ src/Actions/Selection.ts
@@ -0,0 +1,33 @@
+import {window, Selection} from 'vscode';
+
+export class ActionSelection {
+
+    static shrinkToStarts(): Thenable<boolean> {
+        const activeTextEditor = window.activeTextEditor;
+
+        if (! activeTextEditor) {
+            return Promise.resolve(false);
+        }
+
+        activeTextEditor.selections = activeTextEditor.selections.map(selection => {
+            return new Selection(selection.start, selection.start);
+        });
+
+        return Promise.resolve(true);
+    }
+
+    static shrinkToEnds(): Thenable<boolean> {
+        const activeTextEditor = window.activeTextEditor;
+
+        if (! activeTextEditor) {
+            return Promise.resolve(false);
+        }
+
+        activeTextEditor.selections = activeTextEditor.selections.map(selection => {
+            return new Selection(selection.end, selection.end);
+        });
+
+        return Promise.resolve(true);
+    }
+
+};
