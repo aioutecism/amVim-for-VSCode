f531c7b58b63f80c321ed08ddd098d230fcd125b
aioute Gao <aioutecism@gmail.com>
2015-12-16 09:22:44 +0900

    Split word operation to MotionWord.

--- src/Motions/Motion.ts
+++ src/Motions/Motion.ts
@@ -1,19 +1,13 @@
 import {window, Selection, Position} from 'vscode';
 
-export enum MotionWordPosition {NEXT_START, NEXT_END, PREV_START};
-
 export class Motion {
 
 	private lineDelta = 0;
 	private characterDelta = 0;
-	private wordDelta: MotionWordPosition;
 
-	private wordSeparators = './\\\\()"\'\\-:,.;<>~!@#$%^&*|+=\\[\\]{}`~?';
-
-	protected translate(lineDelta: number, characterDelta: number, wordDelta?: MotionWordPosition): void {
+	protected translate(lineDelta: number, characterDelta: number): void {
 		this.lineDelta += lineDelta;
 		this.characterDelta += characterDelta;
-		this.wordDelta = wordDelta;
 	}
 
 	apply(from: Selection): Selection {
@@ -25,7 +19,7 @@ export class Motion {
 
 		const document = activeTextEditor.document;
 
-        // Count tab as editor.tabSize
+		// Count tab as editor.tabSize
 
 		let toLine = from.active.line;
 		toLine += this.lineDelta;
@@ -33,40 +27,7 @@ export class Motion {
 		toLine = Math.min(toLine, document.lineCount - 1);
 
 		let toCharacter = from.active.character;
-
-		if (this.wordDelta !== undefined) {
-			let targetText = document.lineAt(toLine).text;
-
-			if (this.wordDelta === MotionWordPosition.NEXT_START) {
-				targetText = targetText.substr(toCharacter);
-
-				const matches = targetText.match(new RegExp(
-					`^(\\s+)?((?:[${this.wordSeparators}]+|[^\\s${this.wordSeparators}]+)\\s*)?`
-				));
-				toCharacter += matches[1] ? matches[1].length : matches[2] ? matches[2].length : matches[0].length;
-			}
-			else if (this.wordDelta === MotionWordPosition.NEXT_END) {
-				targetText = targetText.substr(toCharacter);
-
-				const matches = targetText.match(new RegExp(
-					`^(\\s+)?((?:[${this.wordSeparators}]+|[^\\s${this.wordSeparators}]+))?`
-				));
-				toCharacter += matches[0].length;
-			}
-			else if (this.wordDelta === MotionWordPosition.PREV_START) {
-				targetText = targetText
-					.substr(0, toCharacter)
-					.split('').reverse().join('');
-
-				const matches = targetText.match(new RegExp(
-					`^(\\s+)?((?:[${this.wordSeparators}]+|[^\\s${this.wordSeparators}]+))?`
-				));
-				toCharacter -= matches[0].length;
-			}
-		}
-
 		toCharacter += this.characterDelta;
-
 		toCharacter = Math.max(toCharacter, 0);
 		toCharacter = Math.min(toCharacter, document.lineAt(toLine).text.length);
 
--- src/Motions/Word.ts
+++ src/Motions/Word.ts
@@ -1,17 +1,74 @@
-import {Motion, MotionWordPosition} from './Motion';
+import {window, Selection, Position} from 'vscode';
+import {Motion} from './Motion';
+
+export enum MotionWordPosition {NEXT_START, NEXT_END, PREV_START};
 
 export class MotionWord extends Motion {
 
+	private wordDelta: MotionWordPosition;
+	private wordSeparators = './\\\\()"\'\\-:,.;<>~!@#$%^&*|+=\\[\\]{}`~?';
+
 	nextStart(): void {
-		this.translate(0, 0, MotionWordPosition.NEXT_START);
+		this.wordDelta = MotionWordPosition.NEXT_START;
 	}
 
 	nextEnd(): void {
-		this.translate(0, 0, MotionWordPosition.NEXT_END);
+		this.wordDelta = MotionWordPosition.NEXT_END;
 	}
 
 	prevStart(): void {
-		this.translate(0, 0, MotionWordPosition.PREV_START);
+		this.wordDelta = MotionWordPosition.PREV_START;
+	}
+
+	apply(from: Selection): Selection {
+		from = super.apply(from);
+
+		const activeTextEditor = window.activeTextEditor;
+
+		if (! activeTextEditor || this.wordDelta === undefined) {
+			return from;
+		}
+
+		const document = activeTextEditor.document;
+
+		let toLine = from.active.line;
+		let toCharacter = from.active.character;
+
+		let targetText = document.lineAt(toLine).text;
+
+		if (this.wordDelta === MotionWordPosition.NEXT_START) {
+			targetText = targetText.substr(toCharacter);
+
+			const matches = targetText.match(new RegExp(
+				`^(\\s+)?((?:[${this.wordSeparators}]+|[^\\s${this.wordSeparators}]+)\\s*)?`
+			));
+			toCharacter += matches[1] ? matches[1].length : matches[2] ? matches[2].length : matches[0].length;
+		}
+
+		else if (this.wordDelta === MotionWordPosition.NEXT_END) {
+			targetText = targetText.substr(toCharacter);
+
+			const matches = targetText.match(new RegExp(
+				`^(\\s+)?((?:[${this.wordSeparators}]+|[^\\s${this.wordSeparators}]+))?`
+			));
+			toCharacter += matches[0].length;
+		}
+
+		else if (this.wordDelta === MotionWordPosition.PREV_START) {
+			targetText = targetText
+				.substr(0, toCharacter)
+				.split('').reverse().join('');
+
+			const matches = targetText.match(new RegExp(
+				`^(\\s+)?((?:[${this.wordSeparators}]+|[^\\s${this.wordSeparators}]+))?`
+			));
+			toCharacter -= matches[0].length;
+		}
+
+		const activePosition = new Position(toLine, toCharacter);
+		const anchorPosition = from.isEmpty ? activePosition : from.anchor;
+
+		return new Selection(anchorPosition, activePosition);
 	}
 
 }
