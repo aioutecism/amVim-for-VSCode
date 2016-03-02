5652c2f8f9ae1035b3b2ffc0ae2f27ad78a5d08d
aioute Gao <aioutecism@gmail.com>
2016-03-03 08:53:04 +0900

    Add layout layer.

---
+++ src/Layouts/Layout.ts
@@ -0,0 +1,36 @@
+import * as US_QWERTY from './US_QWERTY';
+
+export interface LayoutMap {
+    [key: string]: string;
+}
+
+export class Layout {
+
+    private static id: string;
+    private static layouts: {[id: string]: LayoutMap};
+
+    static use(id: string) {
+        if (this.layouts[id]) {
+            this.id = id;
+        }
+        else {
+            throw new Error(`Layout not registered: ${id}`);
+        }
+    }
+
+    static register(id: string, map: LayoutMap) {
+        this.layouts[id] = map;
+    }
+
+    static transformKey(key: string): string {
+        if (this.id) {
+            return this.layouts[this.id][key] || key;
+        }
+        else {
+            return key;
+        }
+    }
+
+}
+
+Layout.register(US_QWERTY.id, US_QWERTY.map);
---
+++ src/Layouts/US_QWERTY.ts
@@ -0,0 +1,41 @@
+import {LayoutMap} from './Layout';
+
+export const id = 'US QWERTY';
+export const map: LayoutMap = {
+    'shift+1' : '!',
+    'shift+2' : '@',
+    'shift+3' : '#',
+    'shift+4' : '$',
+    'shift+5' : '%',
+    'shift+6' : '^',
+    'shift+7' : '&',
+    'shift+8' : '*',
+    'shift+9' : '(',
+    'shift+0' : ')',
+
+    'vk_oem_1' : ';',
+    'vk_oem_2' : '/',
+    'vk_oem_3' : '`',
+    'vk_oem_4' : '[',
+    'vk_oem_5' : '\\',
+    'vk_oem_6' : ']',
+    'vk_oem_7' : '\'',
+
+    'shift+vk_oem_1' : ':',
+    'shift+vk_oem_2' : '?',
+    'shift+vk_oem_3' : '~',
+    'shift+vk_oem_4' : '{',
+    'shift+vk_oem_5' : '|',
+    'shift+vk_oem_6' : '}',
+    'shift+vk_oem_7' : '"',
+
+    'vk_oem_minus'  : '-',
+    'vk_oem_plus'   : '=',
+    'vk_oem_comma'  : ',',
+    'vk_oem_period' : '.',
+
+    'shift+vk_oem_minus'  : '_',
+    'shift+vk_oem_plus'   : '+',
+    'shift+vk_oem_comma'  : '<',
+    'shift+vk_oem_period' : '>',
+};
