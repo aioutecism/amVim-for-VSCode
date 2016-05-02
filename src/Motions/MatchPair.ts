2249a1b7916ea756277dedd27e4cc6621dfa8c56
Daniel Egger <daniel.egger@gmail.com>
2016-05-02 09:10:57 +0200

    look for matchpair-character after cursor in "%" motion

--- src/Motions/MatchPair.ts
+++ src/Motions/MatchPair.ts
@@ -1,15 +1,20 @@
 import {window, Position, Range} from 'vscode';
 import {Motion} from './Motion';
+import {MotionMatch} from '../Motions/Match';
 import {TextObjectBlock} from '../TextObjects/Block';
 
 enum MotionMatchDirection {NEXT, PREV};
 
 export class MotionMatchPair extends Motion {
+    
+    // TODO: C-style comments (/* */) and C preprocessor conditionals are not supported for now
+    
     private static openingCharacterMap = {
         '(': TextObjectBlock.byParentheses,
         '{': TextObjectBlock.byBraces,
         '[': TextObjectBlock.byBrackets,
     }
+    private static openingCharacters = Object.keys(MotionMatchPair.openingCharacterMap);
     
     private static closingCharacterMap = {
         ')': TextObjectBlock.byParentheses,
@@ -20,6 +25,7 @@ export class MotionMatchPair extends Motion {
     private static characterMap = Object.assign(
         {}, MotionMatchPair.openingCharacterMap, MotionMatchPair.closingCharacterMap
     );
+    private static matchCharacters = Object.keys(MotionMatchPair.characterMap);
 
     static matchPair(): Motion {
         const obj = new MotionMatchPair();
@@ -36,20 +42,30 @@ export class MotionMatchPair extends Motion {
         }
 
         const document = activeTextEditor.document;
+        const targetText = document.lineAt(from.line).text;
         
-        const characterUnderCursor = document.getText(new Range(from, new Position(from.line, from.character+1)));
-        if(Object.keys(MotionMatchPair.characterMap).indexOf(characterUnderCursor) < 0) {
+        for(let i = from.character; i < targetText.length; i += 1) {
+            const currentCharacter = targetText[i];
+            
+            if(MotionMatchPair.matchCharacters.indexOf(currentCharacter) < 0) {
+                continue;
+            }
+            
+            const textObject = MotionMatchPair.characterMap[currentCharacter]({isInclusive: true});
+            
+            if(MotionMatchPair.openingCharacters.indexOf(currentCharacter) < 0) {
+                const startRange = textObject.findStartRange(document, new Position(from.line, i));
+                if(startRange !== null) {
+                    return startRange.start;
+                }
+            } else {
+                const endRange = textObject.findEndRange(document, new Position(from.line, i));
+                if(endRange !== null) {
+                    return endRange.start;
+                }
+            }
+            
             return from;
-        }
-        
-        const textObject = MotionMatchPair.characterMap[characterUnderCursor]({isInclusive: true});
-        const startRange = textObject.findStartRange(document, from);
-        const endRange = textObject.findEndRange(document, from);
-        
-        if(Object.keys(MotionMatchPair.openingCharacterMap).indexOf(characterUnderCursor) >= 0) {
-            return endRange.start;
-        } else {
-            return startRange.start;
         }
     }
 }
