332fe2142a6d3cb0485530ee5e77ff584c34a076
aioute Gao <aioutecism@gmail.com>
2016-08-14 22:25:21 +0900

    Refactoring test.

--- test/TextObjects/Word.test.ts
+++ test/TextObjects/Word.test.ts
@@ -1,4 +1,7 @@
 import * as assert from 'assert';
+import * as TestUtil from '../Util';
+import {Position} from 'vscode';
+
 import {TextObjectWord} from '../../src/TextObjects/Word';
 import {Configuration} from '../../src/Configuration';
 
@@ -6,88 +9,22 @@ export function run() {
 
     Configuration.init();
 
-    suite('findPreviousIndexOfDifferentCharacterKind finds correct index of different character', () => {
-        test('with single word on line starting inside of word', (done) => {
-            let line = '  word';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findStartCharacters(line, 4);
-            assert.equal(indexNonInclusive, 2);
-            assert.equal(indexInclusive, 0);
-            done();
-        });
+    test('with single word on line starting inside of word', (done) => {
+        TestUtil.createTempDocument('  word').then(() => {
+            const exclusiveObj = TextObjectWord.byWord({ useBlankSeparatedStyle: false, isInclusive: false });
+            const exclusiveRange = exclusiveObj.apply(new Position(0, 2));
 
-        test('with multiple words starting inside of second word', (done) => {
-            let line = 'word1-word2';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findStartCharacters(line, 9);
-            let result = TextObjectWord.findStartCharacters(line, 9);
-            assert.equal(indexNonInclusive, 6);
-            assert.equal(indexInclusive, 6);
-            done();
-        });
+            assert.equal(exclusiveRange.start.character, 2);
+            assert.equal(exclusiveRange.end.character, 5);
 
-        test('with multiple words starting inside of spaces', (done) => {
-            let line = 'word1    word2';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findStartCharacters(line, 7);
-            assert.equal(indexNonInclusive, 5);
-            assert.equal(indexInclusive, 0);
-            done();
-        });
+            const inclusiveObj = TextObjectWord.byWord({ useBlankSeparatedStyle: false, isInclusive: true });
+            const inclusiveRange = inclusiveObj.apply(new Position(0, 2));
 
-        test('with multiple words starting inside of second word', (done) => {
-            let line = 'word1    word2';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findStartCharacters(line, 11);
-            assert.equal(indexNonInclusive, 9);
-            assert.equal(indexInclusive, 5);
-            done();
-        });
+            assert.equal(inclusiveRange.start.character, 0);
+            assert.equal(inclusiveRange.end.character, 5);
 
-        test('with multiple words not separated by spaces and useBlankSeparatedStyle==true', (done) => {
-            let line = 'word1   word2-word3 word4';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findStartCharacters(line, 16, true);
-            assert.equal(indexNonInclusive, 8);
-            assert.equal(indexInclusive, 5);
             done();
         });
     });
 
-    suite('findNextIndexOfDifferentCharacterKind finds correct index of different character', () => {
-        test('with single word starting inside of word', (done) => {
-            let line = ' word  ';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findEndCharacters(line, 2);
-            assert.equal(indexNonInclusive, 4);
-            assert.equal(indexInclusive, 6);
-            done();
-        });
-
-        test('with multiple words starting inside of second word', (done) => {
-            let line = 'word1-word2  word3';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findEndCharacters(line, 9);
-            assert.equal(indexNonInclusive, 10);
-            assert.equal(indexInclusive, 12);
-            done();
-        });
-
-        test('with multiple words starting inside of second word', (done) => {
-            let line = 'word1    word2';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findEndCharacters(line, 11);
-            assert.equal(indexNonInclusive, 14);
-            assert.equal(indexInclusive, 14);
-            done();
-        });
-
-        test('with multiple words starting inside of spaces', (done) => {
-            let line = 'word1    word2 word3';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findEndCharacters(line, 7);
-            assert.equal(indexNonInclusive, 8);
-            assert.equal(indexInclusive, 13);
-            done();
-        });
-
-        test('with multiple words not separated by spaces and useBlankSeparatedStyle==true', (done) => {
-            let line = 'word1 word2-word3   word4 word5';
-            let [indexInclusive, indexNonInclusive] = TextObjectWord.findEndCharacters(line, 6, true);
-            assert.equal(indexNonInclusive, 16);
-            assert.equal(indexInclusive, 19);
-            done();
-        });
-    });
 }
