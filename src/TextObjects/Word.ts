536fdca309b1c6742d974a5de80f872b4eb8dc09
aioute Gao <aioutecism@gmail.com>
2016-08-14 22:25:06 +0900

    Refactoring TextObjectWord.

--- src/TextObjects/Word.ts
+++ src/TextObjects/Word.ts
@@ -10,7 +10,7 @@ export class TextObjectWord extends TextObject {
         useBlankSeparatedStyle: boolean,
         isInclusive: boolean,
     }) {
-        let obj = new TextObjectWord();
+        const obj = new TextObjectWord();
         obj.isInclusive = args.isInclusive;
         obj.useBlankSeparatedStyle = args.useBlankSeparatedStyle;
 
@@ -19,108 +19,109 @@ export class TextObjectWord extends TextObject {
 
     public findStartRange(document: TextDocument, anchor: Position): Range {
         const lineIndex = anchor.line;
-        const characterIndex = anchor.character;
-        const text = document.lineAt(lineIndex).text;
-        const [startIndexInclusive, startIndexNonInclusive] = TextObjectWord.findStartCharacters(text, characterIndex, this.useBlankSeparatedStyle);
+        const lineText = document.lineAt(lineIndex).text;
 
-        return new Range(lineIndex, startIndexInclusive, lineIndex, startIndexNonInclusive);
+        const anchorCharacterKind = UtilWord.getCharacterKind(
+            lineText.charCodeAt(anchor.character),
+            this.useBlankSeparatedStyle
+        );
+
+        let exclusiveCharacterIndex: number = 0;
+        for (let characterIndex = anchor.character - 1; characterIndex >= 0; characterIndex--) {
+            const characterKind = UtilWord.getCharacterKind(
+                lineText.charCodeAt(characterIndex),
+                this.useBlankSeparatedStyle
+            );
+
+            if (characterKind !== anchorCharacterKind) {
+                exclusiveCharacterIndex = characterIndex + 1;
+                break;
+            }
+        }
+
+        let inclusiveCharacterIndex: number = 0;
+        for (let characterIndex = exclusiveCharacterIndex - 1; characterIndex >= 0; characterIndex--) {
+            const characterKind = UtilWord.getCharacterKind(
+                lineText.charCodeAt(characterIndex),
+                this.useBlankSeparatedStyle
+            );
+
+            if (characterKind !== WordCharacterKind.Blank) {
+                inclusiveCharacterIndex = characterIndex + 1;
+                break;
+            }
+        }
+
+        return new Range(
+            lineIndex, inclusiveCharacterIndex,
+            lineIndex, exclusiveCharacterIndex
+        );
     }
 
     public findEndRange(document: TextDocument, anchor: Position): Range {
         const lineIndex = anchor.line;
-        const characterIndex = anchor.character;
-        const text = document.lineAt(lineIndex).text;
-        const [endIndexInclusive, endIndexNonInclusive] = TextObjectWord.findEndCharacters(text, characterIndex, this.useBlankSeparatedStyle);
+        const lineText = document.lineAt(lineIndex).text;
 
-        return new Range(lineIndex, endIndexNonInclusive + 1, lineIndex, endIndexInclusive + 1);
+        const anchorCharacterKind = UtilWord.getCharacterKind(
+            lineText.charCodeAt(anchor.character),
+            this.useBlankSeparatedStyle
+        );
+
+        let exclusiveCharacterIndex: number = lineText.length - 1;
+        for (let characterIndex = anchor.character + 1; characterIndex < lineText.length; characterIndex++) {
+            const characterKind = UtilWord.getCharacterKind(
+                lineText.charCodeAt(characterIndex),
+                this.useBlankSeparatedStyle
+            );
+
+            if (characterKind !== anchorCharacterKind) {
+                exclusiveCharacterIndex = characterIndex - 1;
+                break;
+            }
+        }
+
+        let inclusiveCharacterIndex: number = lineText.length - 1;
+        if (exclusiveCharacterIndex < lineText.length - 1) {
+            const includeCharacterKind = anchorCharacterKind === WordCharacterKind.Blank
+                ? UtilWord.getCharacterKind(
+                    lineText.charCodeAt(exclusiveCharacterIndex + 1),
+                    this.useBlankSeparatedStyle
+                )
+                : WordCharacterKind.Blank;
+
+            for (let characterIndex = exclusiveCharacterIndex + 1; characterIndex < lineText.length; characterIndex++) {
+                const characterKind = UtilWord.getCharacterKind(
+                    lineText.charCodeAt(characterIndex),
+                    this.useBlankSeparatedStyle
+                );
+
+                if (characterKind !== includeCharacterKind) {
+                    inclusiveCharacterIndex = characterIndex - 1;
+                    break;
+                }
+            }
+        }
+
+        return new Range(
+            lineIndex, exclusiveCharacterIndex + 1,
+            lineIndex, inclusiveCharacterIndex + 1
+        );
     }
 
     protected createRangeDueToIsInclusive(startRange: Range, endRange: Range) {
-        if (!this.isInclusive) {
-            return new Range(startRange.end, endRange.start);
-        }
-        // from vim documentation:
-        // "Any trailing white space is included, unless there is none, then leading white space is included."
-        if (endRange.end.character !== endRange.start.character) {
-            return new Range(startRange.end, endRange.end);
+        if (this.isInclusive) {
+            // From Vim documentation:
+            // "Any trailing white space is included, unless there is none, then leading white space is included."
+            if (! endRange.isEmpty) {
+                return new Range(startRange.end, endRange.end);
+            }
+            else {
+                return new Range(startRange.start, endRange.start);
+            }
         }
         else {
-            return new Range(startRange.start, endRange.start);
+            return new Range(startRange.end, endRange.start);
         }
     }
 
-    static findStartCharacters(text: string, character: number, useBlankSeparatedStyle: boolean = false): [number, number] {
-        const startCharacterKind = UtilWord.getCharacterKind(
-            text.charCodeAt(character), useBlankSeparatedStyle
-        );
-        let inclusiveCharacter = 0;
-        let nonInclusiveCharacter = 0;
-
-        // find nonInclusiveCharacter
-        while (character > 0) {
-            const previousCharacterKind = UtilWord.getCharacterKind(
-                text.charCodeAt(character - 1), useBlankSeparatedStyle
-            );
-            if (previousCharacterKind !== startCharacterKind) {
-                nonInclusiveCharacter = character;
-                inclusiveCharacter = character;
-                break;
-            }
-            character--;
-        }
-
-        // find inclusiveCharacter
-        const allowedCharacterKind = startCharacterKind === WordCharacterKind.Blank ? WordCharacterKind.Regular : WordCharacterKind.Blank;
-        while (character > 0) {
-            const previousCharacterKind = UtilWord.getCharacterKind(
-                text.charCodeAt(character - 1), useBlankSeparatedStyle
-            );
-            if (previousCharacterKind === allowedCharacterKind) {
-                inclusiveCharacter = character - 1;
-            }
-            else {
-                break;
-            }
-            character--;
-        }
-        return [inclusiveCharacter, nonInclusiveCharacter];
-    }
-
-    static findEndCharacters(text: string, character: number, useBlankSeparatedStyle: boolean = false): [number, number] {
-        const startCharacterKind = UtilWord.getCharacterKind(
-            text.charCodeAt(character), useBlankSeparatedStyle
-        );
-        let inclusiveCharacter = text.length;
-        let nonInclusiveCharacter = text.length;
-
-        // find nonInclusiveCharacter
-        while (character < text.length) {
-            const nextCharacterKind = UtilWord.getCharacterKind(
-                text.charCodeAt(character + 1), useBlankSeparatedStyle
-            );
-            if (nextCharacterKind !== startCharacterKind) {
-                nonInclusiveCharacter = character;
-                inclusiveCharacter = character;
-                break;
-            }
-            character++;
-        }
-
-        // find inclusiveCharacter
-        const allowedCharacterKind = startCharacterKind === WordCharacterKind.Blank ? WordCharacterKind.Regular : WordCharacterKind.Blank;
-        while (character < text.length) {
-            const nextCharacterKind = UtilWord.getCharacterKind(
-                text.charCodeAt(character + 1), useBlankSeparatedStyle
-            );
-            if (nextCharacterKind === allowedCharacterKind) {
-                inclusiveCharacter = character + 1;
-            }
-            else {
-                break;
-            }
-            character++;
-        }
-
-        return [inclusiveCharacter, nonInclusiveCharacter];
-    }
 }
