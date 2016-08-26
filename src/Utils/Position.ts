28b1b4c5b8806d592befecd17806d3c533ee9431
aioute Gao <aioutecism@gmail.com>
2016-08-26 21:11:32 +0900

    Add getColumn to UtilPosition.

--- src/Utils/Position.ts
+++ src/Utils/Position.ts
@@ -1,6 +1,21 @@
-import {TextDocument, Position} from 'vscode';
+import {TextEditor, Position} from 'vscode';
 
 export class UtilPosition {
 
+    /**
+     * Get visual column from position.
+     */
+    static getColumn(textEditor: TextEditor, position: Position): number {
+        const text = textEditor.document.lineAt(position.line).text;
+        const tabSize = textEditor.options.tabSize as number;
+
+        let column = 0;
+
+        for (let character = 0; character <= position.character; character++) {
+            column += text.charAt(character) === '\t' ? tabSize : 1;
+        }
+
+        return column;
+    }
 
 }
