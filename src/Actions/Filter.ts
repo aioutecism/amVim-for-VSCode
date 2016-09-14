cb3ec26289a4ec3271a37e96e75707f2be15abaf
aioute Gao <aioutecism@gmail.com>
2016-09-14 21:27:38 +0900

    Add ActionFilter.Format.

---
+++ src/Actions/Filter.ts
@@ -0,0 +1,54 @@
+import {commands, window} from 'vscode';
+import {Motion} from '../Motions/Motion';
+import {ActionMoveCursor} from './MoveCursor';
+import {ActionReveal} from './Reveal';
+
+class Format {
+
+    static bySelections(): Thenable<boolean> {
+        const activeTextEditor = window.activeTextEditor;
+
+        if (! activeTextEditor) {
+            return Promise.resolve(false);
+        }
+
+        if ((activeTextEditor.selections.length === 0)
+        || (activeTextEditor.selections.length === 1 && activeTextEditor.selection.isEmpty)
+        ) {
+            return Promise.resolve(false);
+        }
+
+        return commands.executeCommand('editor.action.format');
+    }
+
+    static byMotions(args: {
+        motions: Motion[]
+    }): Thenable<boolean> {
+        const activeTextEditor = window.activeTextEditor;
+
+        if (! activeTextEditor) {
+            return Promise.resolve(false);
+        }
+
+        const originalSelections = activeTextEditor.selections;
+
+        return ActionMoveCursor.byMotions({
+            motions: args.motions,
+            isVisualMode: true
+        })
+        .then(() => {
+            return commands.executeCommand('editor.action.format');
+        })
+        .then(() => {
+            activeTextEditor.selections = originalSelections;
+            return Promise.resolve(true);
+        })
+        .then(() => ActionReveal.primaryCursor());
+    }
+
+};
+
+
+export const ActionFilter = {
+    Format: Format
+};
