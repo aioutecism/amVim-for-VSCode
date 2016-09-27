71f2a47cfe59b586e4af7b60c36252663ac863ec
aioute Gao <aioutecism@gmail.com>
2016-09-27 14:54:30 +0900

    Add tests.

--- test/Actions/Delete.test.ts
+++ test/Actions/Delete.test.ts
@@ -5,6 +5,7 @@ import {window, Selection} from 'vscode';
 import {Configuration} from '../../src/Configuration';
 import {ActionDelete} from '../../src/Actions/Delete';
 import {MotionWord} from '../../src/Motions/Word';
+import {MotionCharacter} from '../../src/Motions/Character';
 
 export function run() {
 
@@ -26,7 +27,21 @@ export function run() {
                 in: 'Foo end\nBar end',
                 selection: new Selection(1, 0, 1, 0),
                 out: 'Foo end\nBar end',
-            }
+            },
+            {
+                message: 'MotionCharacter.up at first line',
+                motions: [MotionCharacter.up()],
+                in: 'Foo end\nBar end',
+                selection: new Selection(0, 2, 0, 2),
+                out: 'Bar end',
+            },
+            {
+                message: 'MotionCharacter.down at last line',
+                motions: [MotionCharacter.down()],
+                in: 'Foo end\nBar end',
+                selection: new Selection(1, 0, 1, 0),
+                out: 'Foo end',
+            },
         ];
 
         let promise = Promise.resolve();
@@ -41,6 +56,48 @@ export function run() {
                     return ActionDelete.byMotions({
                         motions: testCase.motions
                     }).then(() => {
+                        assert.equal(TestUtil.getDocument().getText(), testCase.out, testCase.message);
+                    });
+                });
+
+            });
+        }
+
+        promise.then(() => {
+            done();
+        }, (error) => {
+            done(error);
+        });
+
+    });
+
+    test('ActionDelete.byLines', (done) => {
+
+        const testCases = [
+            {
+                message: 'Selection at first line',
+                in: 'Foo end\nBar end',
+                selection: new Selection(0, 2, 0, 2),
+                out: 'Bar end',
+            },
+            {
+                message: 'Selection at last line',
+                in: 'Foo end\nBar end',
+                selection: new Selection(1, 0, 1, 0),
+                out: 'Foo end',
+            },
+        ];
+
+        let promise = Promise.resolve();
+
+        while (testCases.length > 0) {
+            const testCase = testCases.shift();
+            promise = promise.then(() => {
+
+                return TestUtil.createTempDocument(testCase.in).then(() => {
+                    TestUtil.setSelection(testCase.selection);
+
+                    return ActionDelete.byLines({}).then(() => {
                         assert.equal(TestUtil.getDocument().getText(), testCase.out, testCase.message);
                     });
                 });
