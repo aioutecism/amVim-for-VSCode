4bda3a5eb8113372c5bca1d917066259280b0e8b
aioute Gao <aioutecism@gmail.com>
2016-10-17 12:02:16 +0900

    Implement ActionReplace.selectionsWithRegister.

--- src/Actions/Replace.ts
+++ src/Actions/Replace.ts
@@ -1,10 +1,40 @@
 import {window, Range} from 'vscode';
 import {StaticReflect} from '../LanguageExtensions/StaticReflect';
 import {SymbolMetadata} from '../Symbols/Metadata';
+import {ActionRegister} from './Register';
+import {ActionSelection} from './Selection';
 import {ActionReveal} from './Reveal';
 import {UtilRange} from '../Utils/Range';
 
 export class ActionReplace {
+
+    @StaticReflect.metadata(SymbolMetadata.Action.isChange, true)
+    static selectionsWithRegister(args: {
+        shouldYank?: boolean
+    }): Thenable<boolean> {
+        args.shouldYank = args.shouldYank === undefined ? false : args.shouldYank;
+
+        const activeTextEditor = window.activeTextEditor;
+
+        if (! activeTextEditor) {
+            return Promise.resolve(false);
+        }
+
+        const document = activeTextEditor.document;
+        const originalTexts: string[] = [];
+
+        return activeTextEditor.edit((editBuilder) => {
+            activeTextEditor.selections.forEach(selection => {
+                originalTexts.push(document.getText(selection));
+                editBuilder.replace(selection, ActionRegister.GetStash());
+            });
+        })
+            .then(() => {
+                ActionRegister.SetStash(originalTexts);
+            })
+            .then(() => ActionSelection.shrinkToActives())
+            .then(() => ActionReveal.primaryCursor());
+    }
 
     @StaticReflect.metadata(SymbolMetadata.Action.isChange, true)
     static selectionsWithCharacter(args: {character: string}): Thenable<boolean> {
