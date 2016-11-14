2263676d764b90f8f6cce66874bfa9c80e5f8ff0
aioute Gao <aioutecism@gmail.com>
2016-11-14 17:35:26 +0900

    New word test.

--- test/TextObjects/Word.test.ts
+++ test/TextObjects/Word.test.ts
@@ -1,174 +1,281 @@
-import * as assert from 'assert';
-import * as TestUtil from '../Util';
-import {Position, Range} from 'vscode';
+import * as BlackBox from '../BlackBox';
 
-import {TextObjectWord} from '../../src/TextObjects/Word';
-import {Configuration} from '../../src/Configuration';
+suite('TextObjectWord: Regular word style', () => {
+    const testCases: BlackBox.TestCase[] = [
+        {
+            from: '[]Foo  bar!@#  end',
+            inputs: 'd i w',
+            to: '[]  bar!@#  end',
+        },
+        {
+            from: '[]Foo  bar!@#  end',
+            inputs: 'd a w',
+            to: '[]bar!@#  end',
+        },
 
-export function run() {
+        {
+            from: 'Fo[]o  bar!@#  end',
+            inputs: 'd i w',
+            to: '[]  bar!@#  end',
+        },
+        {
+            from: 'Fo[]o  bar!@#  end',
+            inputs: 'd a w',
+            to: '[]bar!@#  end',
+        },
 
-    Configuration.init();
+        {
+            from: 'Foo[]  bar!@#  end',
+            inputs: 'd i w',
+            to: 'Foo[]bar!@#  end',
+        },
+        {
+            from: 'Foo[]  bar!@#  end',
+            inputs: 'd a w',
+            to: 'Foo[]!@#  end',
+        },
 
-    test('TextObjectWord: Regular word style', (done) => {
-        TestUtil.createTempDocument('Foo  bar!@#  end').then(() => {
-            const exclusiveObj = TextObjectWord.byWord({ useBlankSeparatedStyle: false, isInclusive: false });
-            const inclusiveObj = TextObjectWord.byWord({ useBlankSeparatedStyle: false, isInclusive: true });
+        {
+            from: 'Foo [] bar!@#  end',
+            inputs: 'd i w',
+            to: 'Foo[]bar!@#  end',
+        },
+        {
+            from: 'Foo [] bar!@#  end',
+            inputs: 'd a w',
+            to: 'Foo[]!@#  end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            // ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 0)), new Range(0, 0, 0, 3));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 0)), new Range(0, 0, 0, 5));
+        {
+            from: 'Foo  []bar!@#  end',
+            inputs: 'd i w',
+            to: 'Foo  []!@#  end',
+        },
+        {
+            from: 'Foo  []bar!@#  end',
+            inputs: 'd a w',
+            to: 'Foo[]!@#  end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //   ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 2)), new Range(0, 0, 0, 3));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 2)), new Range(0, 0, 0, 5));
+        {
+            from: 'Foo  ba[]r!@#  end',
+            inputs: 'd i w',
+            to: 'Foo  []!@#  end',
+        },
+        {
+            from: 'Foo  ba[]r!@#  end',
+            inputs: 'd a w',
+            to: 'Foo[]!@#  end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //    ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 3)), new Range(0, 3, 0, 5));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 3)), new Range(0, 3, 0, 8));
+        {
+            from: 'Foo  bar[]!@#  end',
+            inputs: 'd i w',
+            to: 'Foo  bar[]  end',
+        },
+        {
+            from: 'Foo  bar[]!@#  end',
+            inputs: 'd a w',
+            to: 'Foo  bar[]end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //     ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 4)), new Range(0, 3, 0, 5));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 4)), new Range(0, 3, 0, 8));
+        {
+            from: 'Foo  bar!@[]#  end',
+            inputs: 'd i w',
+            to: 'Foo  bar[]  end',
+        },
+        {
+            from: 'Foo  bar!@[]#  end',
+            inputs: 'd a w',
+            to: 'Foo  bar[]end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //      ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 5)), new Range(0, 5, 0, 8));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 5)), new Range(0, 3, 0, 8));
+        {
+            from: 'Foo  bar!@#[]  end',
+            inputs: 'd i w',
+            to: 'Foo  bar!@#[]end',
+        },
+        {
+            from: 'Foo  bar!@#[]  end',
+            inputs: 'd a w',
+            to: 'Foo  bar!@[]#',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //        ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 7)), new Range(0, 5, 0, 8));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 7)), new Range(0, 3, 0, 8));
+        {
+            from: 'Foo  bar!@# [] end',
+            inputs: 'd i w',
+            to: 'Foo  bar!@#[]end',
+        },
+        {
+            from: 'Foo  bar!@# [] end',
+            inputs: 'd a w',
+            to: 'Foo  bar!@[]#',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //         ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 8)), new Range(0, 8, 0, 11));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 8)), new Range(0, 8, 0, 13));
+        {
+            from: 'Foo  bar!@#  []end',
+            inputs: 'd i w',
+            to: 'Foo  bar!@# [] ',
+        },
+        {
+            from: 'Foo  bar!@#  []end',
+            inputs: 'd a w',
+            to: 'Foo  bar!@[]#',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //           ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 10)), new Range(0, 8, 0, 11));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 10)), new Range(0, 8, 0, 13));
+        {
+            from: 'Foo  bar!@#  en[]d',
+            inputs: 'd i w',
+            to: 'Foo  bar!@# [] ',
+        },
+        {
+            from: 'Foo  bar!@#  en[]d',
+            inputs: 'd a w',
+            to: 'Foo  bar!@[]#',
+        },
+    ];
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //            ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 11)), new Range(0, 11, 0, 13));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 11)), new Range(0, 11, 0, 16));
+    for (let i = 0; i < testCases.length; i++) {
+        BlackBox.run(testCases[i]);
+    }
+});
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //             ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 12)), new Range(0, 11, 0, 13));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 12)), new Range(0, 11, 0, 16));
+suite('TextObjectWord: Blank separated word style', () => {
+    const testCases: BlackBox.TestCase[] = [
+        {
+            from: '[]Foo  bar!@#  end',
+            inputs: 'd i W',
+            to: '[]  bar!@#  end',
+        },
+        {
+            from: '[]Foo  bar!@#  end',
+            inputs: 'd a W',
+            to: '[]bar!@#  end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //              ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 13)), new Range(0, 13, 0, 16));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 13)), new Range(0, 11, 0, 16));
+        {
+            from: 'Fo[]o  bar!@#  end',
+            inputs: 'd i W',
+            to: '[]  bar!@#  end',
+        },
+        {
+            from: 'Fo[]o  bar!@#  end',
+            inputs: 'd a W',
+            to: '[]bar!@#  end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //                ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 15)), new Range(0, 13, 0, 16));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 15)), new Range(0, 11, 0, 16));
+        {
+            from: 'Foo[]  bar!@#  end',
+            inputs: 'd i W',
+            to: 'Foo[]bar!@#  end',
+        },
+        {
+            from: 'Foo[]  bar!@#  end',
+            inputs: 'd a W',
+            to: 'Foo[]  end',
+        },
 
-            done();
-        });
-    });
+        {
+            from: 'Foo [] bar!@#  end',
+            inputs: 'd i W',
+            to: 'Foo[]bar!@#  end',
+        },
+        {
+            from: 'Foo [] bar!@#  end',
+            inputs: 'd a W',
+            to: 'Foo[]  end',
+        },
 
-    test('TextObjectWord: Blank separated word style', (done) => {
-        TestUtil.createTempDocument('Foo  bar!@#  end').then(() => {
-            const exclusiveObj = TextObjectWord.byWord({ useBlankSeparatedStyle: true, isInclusive: false });
-            const inclusiveObj = TextObjectWord.byWord({ useBlankSeparatedStyle: true, isInclusive: true });
+        {
+            from: 'Foo  []bar!@#  end',
+            inputs: 'd i W',
+            to: 'Foo  []  end',
+        },
+        {
+            from: 'Foo  []bar!@#  end',
+            inputs: 'd a W',
+            to: 'Foo  []end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            // ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 0)), new Range(0, 0, 0, 3));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 0)), new Range(0, 0, 0, 5));
+        {
+            from: 'Foo  ba[]r!@#  end',
+            inputs: 'd i W',
+            to: 'Foo  []  end',
+        },
+        {
+            from: 'Foo  ba[]r!@#  end',
+            inputs: 'd a W',
+            to: 'Foo  []end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //   ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 2)), new Range(0, 0, 0, 3));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 2)), new Range(0, 0, 0, 5));
+        {
+            from: 'Foo  bar[]!@#  end',
+            inputs: 'd i W',
+            to: 'Foo  []  end',
+        },
+        {
+            from: 'Foo  bar[]!@#  end',
+            inputs: 'd a W',
+            to: 'Foo  []end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //    ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 3)), new Range(0, 3, 0, 5));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 3)), new Range(0, 3, 0, 11));
+        {
+            from: 'Foo  bar!@[]#  end',
+            inputs: 'd i W',
+            to: 'Foo  []  end',
+        },
+        {
+            from: 'Foo  bar!@[]#  end',
+            inputs: 'd a W',
+            to: 'Foo  []end',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //     ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 4)), new Range(0, 3, 0, 5));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 4)), new Range(0, 3, 0, 11));
+        {
+            from: 'Foo  bar!@#[]  end',
+            inputs: 'd i W',
+            to: 'Foo  bar!@#[]end',
+        },
+        {
+            from: 'Foo  bar!@#[]  end',
+            inputs: 'd a W',
+            to: 'Foo  bar!@[]#',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //      ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 5)), new Range(0, 5, 0, 11));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 5)), new Range(0, 5, 0, 13));
+        {
+            from: 'Foo  bar!@# [] end',
+            inputs: 'd i W',
+            to: 'Foo  bar!@#[]end',
+        },
+        {
+            from: 'Foo  bar!@# [] end',
+            inputs: 'd a W',
+            to: 'Foo  bar!@[]#',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //        ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 7)), new Range(0, 5, 0, 11));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 7)), new Range(0, 5, 0, 13));
+        {
+            from: 'Foo  bar!@#  []end',
+            inputs: 'd i W',
+            to: 'Foo  bar!@# [] ',
+        },
+        {
+            from: 'Foo  bar!@#  []end',
+            inputs: 'd a W',
+            to: 'Foo  bar!@[]#',
+        },
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //         ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 8)), new Range(0, 5, 0, 11));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 8)), new Range(0, 5, 0, 13));
+        {
+            from: 'Foo  bar!@#  en[]d',
+            inputs: 'd i W',
+            to: 'Foo  bar!@# [] ',
+        },
+        {
+            from: 'Foo  bar!@#  en[]d',
+            inputs: 'd a W',
+            to: 'Foo  bar!@[]#',
+        },
+    ];
 
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //           ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 10)), new Range(0, 5, 0, 11));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 10)), new Range(0, 5, 0, 13));
-
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //            ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 11)), new Range(0, 11, 0, 13));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 11)), new Range(0, 11, 0, 16));
-
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //             ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 12)), new Range(0, 11, 0, 13));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 12)), new Range(0, 11, 0, 16));
-
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //              ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 13)), new Range(0, 13, 0, 16));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 13)), new Range(0, 11, 0, 16));
-
-            // Foo  bar!@#  end
-            // 0123456789012345
-            //                ^
-            assert.deepEqual(exclusiveObj.apply(new Position(0, 15)), new Range(0, 13, 0, 16));
-            assert.deepEqual(inclusiveObj.apply(new Position(0, 15)), new Range(0, 11, 0, 16));
-
-            done();
-        });
-    });
-
-}
+    for (let i = 0; i < testCases.length; i++) {
+        BlackBox.run(testCases[i]);
+    }
+});
