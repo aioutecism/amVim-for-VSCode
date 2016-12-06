3fa860faafbc8d2538427796c338addae0f71c40
aioute Gao <aioutecism@gmail.com>
2016-12-06 15:23:24 +0900

    Handle preferred relative range in ActionIndent.

--- src/Actions/Indent.ts
+++ src/Actions/Indent.ts
@@ -46,6 +46,7 @@ export class ActionIndent {
         indentLevelOffset: number,
         isVisualMode?: boolean,
         isVisualLineMode?: boolean,
+        preferedRelativeRange?: Range,
     }): Thenable<boolean> {
         args.isVisualMode = args.isVisualMode === undefined ? false : args.isVisualMode;
         args.isVisualLineMode = args.isVisualLineMode === undefined ? false : args.isVisualLineMode;
@@ -60,15 +61,30 @@ export class ActionIndent {
         const indentUnit = ActionIndent.getIndentUnit();
 
         const lineNumbers: {[lineNumber: number]: boolean} = {};
-        activeTextEditor.selections.forEach(selection => {
-            for (let i = selection.start.line; i <= selection.end.line; i++) {
-                lineNumbers[i] = true;
-            }
-        });
+
+        if (args.preferedRelativeRange) {
+            activeTextEditor.selections.forEach(selection => {
+                for (let i = selection.active.line; i <= selection.active.line + args.preferedRelativeRange!.end.line; i++) {
+                    lineNumbers[i] = true;
+                }
+            });
+        }
+        else {
+            activeTextEditor.selections.forEach(selection => {
+                for (let i = selection.start.line; i <= selection.end.line; i++) {
+                    lineNumbers[i] = true;
+                }
+            });
+        }
 
         return window.activeTextEditor.edit((editBuilder) => {
             Object.keys(lineNumbers).forEach(key => {
                 const lineNumber = parseInt(key, 10);
+
+                if (lineNumber < 0 || lineNumber >= document.lineCount) {
+                    return;
+                }
+
                 const line = document.lineAt(lineNumber);
 
                 const currentIndentLevel = ActionIndent.getIndentLevel(lineNumber);
@@ -104,6 +120,7 @@ export class ActionIndent {
     static increase(args: {
         isVisualMode?: boolean,
         isVisualLineMode?: boolean,
+        preferedRelativeRange?: Range,
     }): Thenable<boolean> {
         return ActionIndent.changeIndentLevel(Object.assign({
             indentLevelOffset: +1,
@@ -114,6 +131,7 @@ export class ActionIndent {
     static decrease(args: {
         isVisualMode?: boolean,
         isVisualLineMode?: boolean,
+        preferedRelativeRange?: Range,
     }): Thenable<boolean> {
         return ActionIndent.changeIndentLevel(Object.assign({
             indentLevelOffset: -1,
