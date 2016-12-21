198cf850654dd6562953c02e0fe91ba8ba8c9a47
aioute Gao <aioutecism@gmail.com>
2016-12-21 15:50:20 +0900

    Add timeout to avoid null activeTextEditor.

--- src/Actions/BlockCursor.ts
+++ src/Actions/BlockCursor.ts
@@ -3,33 +3,41 @@ import {window, TextEditorCursorStyle} from 'vscode';
 export class ActionBlockCursor {
 
     static on(): Thenable<boolean> {
-        const activeTextEditor = window.activeTextEditor;
+        return new Promise((resovle) => {
+            setTimeout(() => {
+                const activeTextEditor = window.activeTextEditor;
 
-        if (! activeTextEditor) {
-            return Promise.resolve(false);
-        }
+                if (! activeTextEditor) {
+                    return Promise.resolve(false);
+                }
 
-        // Workaround for VSCode API's bug: https://github.com/Microsoft/vscode/issues/17513
-        // TODO: Remove next line when the bug is fixed.
-        activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Line;
-        activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Block;
+                // Workaround for VSCode API's bug: https://github.com/Microsoft/vscode/issues/17513
+                // TODO: Remove next line when the bug is fixed.
+                activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Underline;
+                activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Block;
 
-        return Promise.resolve(true);
+                resovle(true);
+            }, 0);
+        });
     }
 
     static off(): Thenable<boolean> {
-        const activeTextEditor = window.activeTextEditor;
+        return new Promise((resovle) => {
+            setTimeout(() => {
+                const activeTextEditor = window.activeTextEditor;
 
-        if (! activeTextEditor) {
-            return Promise.resolve(false);
-        }
+                if (! activeTextEditor) {
+                    return Promise.resolve(false);
+                }
 
-        // Workaround for VSCode API's bug: https://github.com/Microsoft/vscode/issues/17513
-        // TODO: Remove next line when the bug is fixed.
-        activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Block;
-        activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Line;
+                // Workaround for VSCode API's bug: https://github.com/Microsoft/vscode/issues/17513
+                // TODO: Remove next line when the bug is fixed.
+                activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Underline;
+                activeTextEditor.options.cursorStyle = TextEditorCursorStyle.Line;
 
-        return Promise.resolve(true);
+                resovle(true);
+            }, 0);
+        });
     }
 
 }
