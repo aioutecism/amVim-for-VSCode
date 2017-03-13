b9c956a0d9a97af98ea3cfa57f78be194bdad332
aioute Gao <aioutecism@gmail.com>
2017-03-13 13:25:18 +0900

    Added tests for ~.

---
+++ test/ModeNormal/~.test.ts
@@ -0,0 +1,40 @@
+import * as BlackBox from '../Framework/BlackBox';
+
+suite.only('Normal: ~', () => {
+    const testCases: BlackBox.TestCase[] = [
+        {
+            from: '[]Foo',
+            inputs: '~',
+            to: 'f[]oo',
+        },
+        {
+            from: 'f[]oo',
+            inputs: '~',
+            to: 'fO[]o',
+        },
+        {
+            from: 'fO[]o',
+            inputs: '~',
+            to: 'fO[]O',
+        },
+        {
+            from: '[]åäö',
+            inputs: '~',
+            to: 'Å[]äö',
+        },
+        {
+            from: 'Å[]äö',
+            inputs: '~',
+            to: 'ÅÄ[]ö',
+        },
+        {
+            from: 'ÅÄ[]ö',
+            inputs: '~',
+            to: 'ÅÄ[]Ö',
+        },
+    ];
+
+    for (let i = 0; i < testCases.length; i++) {
+        BlackBox.run(testCases[i]);
+    }
+});
