40603cc06a4827135a706957963e9320ababc01c
Teja Sophista <tejanium@yahoo.com>
2020-05-31 18:14:34 +0100

    adjust with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
     target: 'node',
     entry: './src/extension.ts',
     output: {
-        path: path.resolve(__dirname, 'out/src'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'extension.js',
         libraryTarget: 'commonjs2',
         devtoolModuleFilenameTemplate: '../[resource-path]',
