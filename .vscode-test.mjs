1bf46060e22665d3e0e73277622d5c2204d729b8
Alison Winters <alisonatwork@outlook.com>
2024-02-28 18:48:35 +0800

    bump vscode engine to 1.75.0

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
     files: 'out/test/**/*.test.js',
-    version: '1.74.0',
+    version: '1.75.0',
     mocha: {
         ui: 'tdd',
         timeout: 2500,
